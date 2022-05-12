3ac687e9bc08005fecc67db5fa5d37b8cdcf51fd
Ilnur <ilnrzakirov@gmail.com>
2022-05-12 11:59:08 +0300

    ex04

--- day01/ex04/state.py
+++ day01/ex04/state.py
@@ -1,4 +1,23 @@
+import sys
+
+
+def capital(argv):
+    if len(argv) != 2:
+        return 1
+    states = {
+        "Oregon": "OR",
+        "Alabama": "AL",
+        "New Jersey": "NJ",
+        "Colorado": "CO"
+    }
+
+    capital_cities = {
+        "OR": "Salem",
+        "AL": "Montgomery",
+        "NJ": "Trenton",
+        "CO": "Denver"
+    }
 
 
 if __name__ == '__main__':
-    state()
+    capital(sys.argv)
