8f204abc5f705e121c622b1b0a025add1d070f24
Ilnur <ilnrzakirov@gmail.com>
2022-05-12 12:02:24 +0300

    ex04

--- day01/ex04/state.py
+++ day01/ex04/state.py
@@ -17,6 +17,17 @@ def capital(argv):
         "NJ": "Trenton",
         "CO": "Denver"
     }
+    input_value = argv[1]
+    key_for_capital_cities = ""
+    for key, value in capital_cities.items():
+        if value == input_value:
+            key_for_capital_cities = key
+            break
+    for key, value in states.items():
+        if key_for_capital_cities == value:
+            print(key)
+            return 0
+    print("Unknown state")
 
 
 if __name__ == '__main__':
