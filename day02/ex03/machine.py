5773b95c5cd44433ada96879b21b4d7ff3bc3063
Ilnur <ilnrzakirov@gmail.com>
2022-05-14 21:44:24 +0300

    ex03

--- day02/ex03/machine.py
+++ day02/ex03/machine.py
@@ -7,8 +7,10 @@ class CoffeeMachine:
         self.count = 0
 
     class EmptyCup(beverages.HotBeverage):
-        price = 0.90
-        name = "empty cup"
+        def __init__(self):
+            super().__init__()
+            self.price = 0.90
+            self.name = "empty cup"
 
         def description(self):
             return "An empty cup?! Gimme my money back!"
@@ -22,3 +24,28 @@ class CoffeeMachine:
         self.count = 0
         print("Machine repaired")
 
+    def serve(self, beverage):
+        if self.count > 9:
+            raise self.BrokenMachineException
+        self.count += 1
+        rand = random.randint(0, 1)
+        if rand == 1:
+            return self.EmptyCup()
+        return beverage()
+
+
+def main():
+    coffee = CoffeeMachine()
+    for i in range(0, 10):
+        try:
+            print(coffee.serve(beverages.Coffee))
+            print(coffee.serve(beverages.Tea))
+            print(coffee.serve(beverages.Cappuccino))
+            print(coffee.serve(beverages.Chocolate))
+        except CoffeeMachine.BrokenMachineException as error:
+            print(error)
+            coffee.repair()
+
+
+if __name__ == "__main__":
+    main()
