05db37149f57923582ad73fa3b1adf6e46c902f9
Ilnur <ilnrzakirov@gmail.com>
2022-05-14 21:26:00 +0300

    ex03

--- day02/ex03/machine.py
+++ day02/ex03/machine.py
@@ -9,5 +9,16 @@ class CoffeeMachine:
     class EmptyCup(beverages.HotBeverage):
         price = 0.90
         name = "empty cup"
+
         def description(self):
             return "An empty cup?! Gimme my money back!"
+
+    class BrokenMachineException(Exception):
+        def __init__(self):
+            msg = "This coffee machine has to be repaired."
+            super().__init__(self, msg)
+
+    def repair(self):
+        self.count = 0
+        print("Machine repaired")
+
