3026fe4c62836d126254b089928f4e7bfa85fa3d
Ilnur <ilnrzakirov@gmail.com>
2022-05-14 21:23:09 +0300

    ex03

--- day02/ex03/machine.py
+++ day02/ex03/machine.py
@@ -1,6 +1,13 @@
 import beverages
+import random
 
 
 class CoffeeMachine:
     def __init__(self):
         self.count = 0
+
+    class EmptyCup(beverages.HotBeverage):
+        price = 0.90
+        name = "empty cup"
+        def description(self):
+            return "An empty cup?! Gimme my money back!"
