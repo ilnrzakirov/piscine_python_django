32eea00e1d9ca97d1f2d48fe29592da26140b359
Ilnur <ilnrzakirov@gmail.com>
2022-05-15 20:07:16 +0300

    ex06

--- day02/ex06/Page.py
+++ day02/ex06/Page.py
@@ -1,5 +1,5 @@
 import elements
-from elem import Elem
+from elem import Elem, Text
 
 
 class Page:
@@ -8,3 +8,9 @@ class Page:
             raise Elem.ValidationError
         self.elem = elem
 
+    def is_valid(self):
+        if not (isinstance(self.elem, (elements.Html, elements.Head, elements.Body, elements.Title, elements.Meta,
+                                 elements.Img, elements.Table, elements.Th, elements.Tr, elements.Td, elements.Ul,
+                                 elements.Ol, elements.Li, elements.H1, elements.H2, elements.P, elements.Div,
+                                 elements.Span, elements.Hr, elements.Br)) or type(self.elem) == Text):
+            return False
