6a2c51be43dfca277c6b3238ebb907e9a9aaaf2e
Ilnur <ilnrzakirov@gmail.com>
2022-05-15 18:38:47 +0300

    ex05

--- day02/ex05/elements.py
+++ day02/ex05/elements.py
@@ -1,6 +1,15 @@
 from elem import Elem
 
 
-class HTML(Elem):
-    def __init__(self, content=None, attr: dict = {}):
+class Html(Elem):
+    def __init__(self, content=None, attr={}):
         super().__init__(tag='html', attr=attr, content=content, tag_type='double')
+
+
+class Head(Elem):
+    def __init__(self, content=None, attr={}):
+        super().__init__(tag='head', attr=attr, content=content, tag_type='double')
+
+class Meta(Elem):
+    def __init__(self, content=None, attr: dict = {}):
+        super().__init__(tag='meta', attr=attr, content=content, tag_type='simple')
