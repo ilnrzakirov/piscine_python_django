95c1a55fb79fb1f2f52785b20e1b377253e44034
Ilnur <ilnrzakirov@gmail.com>
2022-05-15 20:07:53 +0300

    ex06

--- day02/ex06/Page.py
+++ day02/ex06/Page.py
@@ -10,7 +10,7 @@ class Page:
 
     def is_valid(self):
         if not (isinstance(self.elem, (elements.Html, elements.Head, elements.Body, elements.Title, elements.Meta,
-                                 elements.Img, elements.Table, elements.Th, elements.Tr, elements.Td, elements.Ul,
-                                 elements.Ol, elements.Li, elements.H1, elements.H2, elements.P, elements.Div,
-                                 elements.Span, elements.Hr, elements.Br)) or type(self.elem) == Text):
+                                       elements.Img, elements.Table, elements.Th, elements.Tr, elements.Td, elements.Ul,
+                                       elements.Ol, elements.Li, elements.H1, elements.H2, elements.P, elements.Div,
+                                       elements.Span, elements.Hr, elements.Br)) or type(self.elem) == Text):
             return False
