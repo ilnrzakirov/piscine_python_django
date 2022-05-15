322a11037a63168db2dab3cb1ecdbbbd7eeb4eeb
Ilnur <ilnrzakirov@gmail.com>
2022-05-15 20:01:12 +0300

    ex06

--- day02/ex06/Page.py
+++ day02/ex06/Page.py
@@ -1,3 +1,10 @@
 import elements
+from elem import Elem
+
 
 class Page:
+    def __init__(self, elem):
+        if not isinstance(elem, Elem):
+            raise Elem.ValidationError
+        self.elem = elem
+
