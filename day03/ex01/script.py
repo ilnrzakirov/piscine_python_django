27a2fe8b9951865d72969cf98160073b20c80329
bcarlee <bcarlee@student.21-school.ru>
2022-05-16 13:07:46 +0300

    ex01

--- day03/ex01/script.py
+++ day03/ex01/script.py
@@ -2,9 +2,12 @@ from local_lib.path import Path
 
 if __name__ == '__main__':
     d = Path('script')
-    d.mkdir()
+    try:
+        d.mkdir()
+    except FileExistsError as error:
+        pass
     file = Path("script/script.txt")
     file.touch()
-    file.write_text("Hello world")
+    file.write_text("Hello world!")
     data =file.read_text()
     print(data)
