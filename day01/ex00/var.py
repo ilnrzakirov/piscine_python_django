ae29cf3a8c8d71686f5e2e5d2f33f6d9a57b68f7
bcarlee <bcarlee@student.21-school.ru>
2022-05-16 16:17:45 +0300

    ex02

--- day01/ex00/var.py
+++ day01/ex00/var.py
@@ -1,22 +1,22 @@
 def my_var():
     i = 42
-    print(f"{i} est de type {type(i)}")
+    print(f"{i} has a type {type(i)}")
     i = "42"
-    print(f"{i} est de type {type(i)}")
+    print(f"{i} has a type {type(i)}")
     i = "quarante-deux"
-    print(f"{i} est de type {type(i)}")
+    print(f"{i} has a type {type(i)}")
     i = 42.0
-    print(f"{i} est de type {type(i)}")
+    print(f"{i} has a type {type(i)}")
     i = True
-    print(f"{i} est de type {type(i)}")
+    print(f"{i} has a type {type(i)}")
     i = [42]
-    print(f"{i} est de type {type(i)}")
+    print(f"{i} has a type {type(i)}")
     i = {42: 42}
-    print(f"{i} est de type {type(i)}")
+    print(f"{i} has a type {type(i)}")
     i = (42,)
-    print(f"{i} est de type {type(i)}")
+    print(f"{i} has a type {type(i)}")
     i = set()
-    print(f"{i} est de type {type(i)}")
+    print(f"{i} has a type {type(i)}")
 
 if __name__ == '__main__':
     my_var()
