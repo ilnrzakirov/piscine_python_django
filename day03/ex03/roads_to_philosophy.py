7a28d13a445a81ed05d98bc1ae5a28a8cb9479b9
Ilnur <ilnrzakirov@gmail.com>
2022-05-16 20:46:27 +0300

    ex06

--- day03/ex03/roads_to_philosophy.py
+++ day03/ex03/roads_to_philosophy.py
@@ -21,19 +21,18 @@ def search_phil(word):
     data = BeautifulSoup(response.text, 'html.parser')
     title = data.find(id='firstHeading').text
     if title in titleList:
-        print("its loop")
         return
     titleList.append(title)
     print(title)
     if title == "Philosophy":
-        print(f"{len(titleList)} to Philosophy")
+        print(f"{len(titleList)} roads from to Philosophy")
+        sys.exit()
     content = data.find(id="mw-content-text")
     links = content.select("p > a")
     for link in links:
         if link.get('href') is not None and link['href'].startswith('/wiki/') \
                 and not link['href'].startswith('/wiki/Wikipedia:') and not link['href'].startswith('/wiki/Help:'):
             search_phil(link["href"])
-            return
     print("It's a dead end !")
     return
 
