c8ff367750d28bdedc0fba2ef79162b823297fa7
Ilnur <ilnrzakirov@gmail.com>
2022-05-16 20:39:16 +0300

    ex06

--- day03/ex03/roads_to_philosophy.py
+++ day03/ex03/roads_to_philosophy.py
@@ -1,7 +1,49 @@
+import sys
+
 import requests
 import dewiki
+from bs4 import BeautifulSoup
+
+titleList = []
+def search_phil(word):
+    if word.startswith("/wiki/"):
+        word = word.replace("/wiki/", "")
+    url = f'https://en.wikipedia.org/wiki/{word}'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.HTTPError as error:
+        if response.status_code == 404:
+            print("page not found")
+            return
+        print(error)
+        return
+    data = BeautifulSoup(response.text, 'html.parser')
+    title = data.find(id='firstHeading').text
+    if title in titleList:
+        print("its loop")
+        return
+    titleList.append(title)
+    print(title)
+    if title == "Philosophy":
+        print(f"{len(titleList)} to Philosophy")
+    content = data.find(id="mw-content-text")
+    links = content.select("p > a")
+    for link in links:
+        if link.get('href') is not None and link['href'].startswith('/wiki/') \
+                and not link['href'].startswith('/wiki/Wikipedia:') and not link['href'].startswith('/wiki/Help:'):
+            search_phil(link["href"])
+            return
+    print("It's a dead end !")
+    return
+
 
 def main():
+    if len(sys.argv) != 2:
+        print("Bad argument")
+        sys.exit()
+    if len(sys.argv) == 2:
+        search_phil(sys.argv[1])
     
 
 if __name__ == '__main__':
