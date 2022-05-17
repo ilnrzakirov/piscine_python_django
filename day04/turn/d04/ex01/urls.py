535c94a18cfc38c9b9980f0a35ba3c81b9b0de5c
bcarlee <bcarlee@student.21-school.ru>
2022-05-17 16:32:02 +0300

    tern file

--- day04/turn/d04/ex01/urls.py
+++ day04/turn/d04/ex01/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/django/', views.django_view, name='django'),
-    path('/display/', views.display_view, name='display'),
-    path('/templates/', views.templates_view, name='templates'),
+    path('django/', views.django_view, name='django'),
+    path('display/', views.display_view, name='display'),
+    path('templates/', views.templates_view, name='templates'),
 ]
