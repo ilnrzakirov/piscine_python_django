a8bdb123c9f2996b0ef4c767e7c040d85eface44
bcarlee <bcarlee@student.21-school.ru>
2022-05-17 10:44:54 +0300

    ex02

--- day04/ex00/script.sh
+++ day04/ex00/script.sh
@@ -3,6 +3,6 @@
 python3 -m venv django_venv
 source django_venv/bin/activate
 django_venv/bin/pip3 install --force-reinstall -r requirement.txt
-django-admin startproject hello_world
-cd hello_world
-django-admin startapp hello
+django-admin startproject d04
+cd d04
+django-admin startapp ex00
