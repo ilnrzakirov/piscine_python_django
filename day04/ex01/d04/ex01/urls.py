7fbe1e1b9c280956c30e150ab265d5c01229bce7
bcarlee <bcarlee@student.21-school.ru>
2022-05-17 12:35:03 +0300

    ex01

--- day04/ex01/d04/ex01/urls.py
+++ day04/ex01/d04/ex01/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('ex01/django', views.django_view),
-    path('ex01/display', views.display_view),
-    path('ex01/templates', views.templates_view),
+    path('ex01/django', views.django_view, name='django'),
+    path('ex01/display', views.display_view, name='display'),
+    path('ex01/templates', views.templates_view, name='templates'),
 ]
