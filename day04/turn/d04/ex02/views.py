fdc2d51fd77ef896f8eac775b152291619f59e40
bcarlee <bcarlee@student.21-school.ru>
2022-05-17 16:24:50 +0300

    tern file

--- day04/turn/d04/ex02/views.py
+++ day04/turn/d04/ex02/views.py
@@ -29,6 +29,7 @@ class SubmitView(FormView):
                 res.append(line)
             return render(request, 'submit.html', context={'text': res, 'form': form})
 
+
 def history_view(request):
     print(settings.LOG_ROOT)
     with open(settings.LOG_ROOT, "r") as file:
