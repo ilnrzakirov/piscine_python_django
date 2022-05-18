b3296c6f0cd9ca5d09ba5e85205cd06ea088046c
bcarlee <bcarlee@student.21-school.ru>
2022-05-18 14:08:59 +0300

    ex02

--- day05/Django/ex02/views.py
+++ day05/Django/ex02/views.py
@@ -29,7 +29,15 @@ def populate_view(request):
                                    host=db['HOST'], port=db['PORT'])
         with connect.cursor() as db_connect:
             db_connect.execute("INSERT INTO ex02_movies (episode_nb, title, opening_crawl, director, producer, "
-                               "release_date)")
+                               "release_date) VALUES (1, 'The Phantom Menace', 'George Lucas', 'Rick McCallum', "
+                               "'1999-05-19'), (2, 'Attack of the Clones', 'George Lucas', 'Rick McCallum', "
+                               "'2002-05-16'), (3, 'Revenge of the Sith', 'George Lucas', 'Rick McCallum', "
+                               "'2005-05-19'), (4, 'A New Hope', 'George Lucas', 'Gary Kurtz, Rick McCallum', "
+                               "'1977-05-25'), (5, 'The Empire Strikes Back', 'Irvin Kershner', "
+                               "'Gary Kurtz, Rick McCallum', '1980-05-17'), (6, 'Return of the Jedi', "
+                               "'Richard Marquand ', 'Howard G. Kazanjian, George Lucas, Rick McCallum', '1983-05-25'),"
+                               "(7, 'The Force Awakens', 'J. J. Abrams', 'Kathleen Kennedy, J. J. Abrams, Bryan Burk', "
+                               "'2015-12-11');")
             connect.commit()
             return HttpResponse("Ok")
     except Exception as error:
