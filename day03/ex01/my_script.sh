bb88d817bb45f64dc17f5a09cf0cd9df91e0a656
bcarlee <bcarlee@student.21-school.ru>
2022-05-18 15:36:07 +0300

    ex02

--- day03/ex01/my_script.sh
+++ day03/ex01/my_script.sh
@@ -4,4 +4,4 @@ python3 -m venv local_lib
 source local_lib/bin/activate
 pip3 --version
 pip3 install -t local_lib --upgrade git+https://github.com/jaraco/path.py.git > log.log
-python3 script.py
+python3 my_program.py
