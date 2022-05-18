eed31a21224d3b00ba77fe9d6b1c1e3150198f60
Ilnur <ilnrzakirov@gmail.com>
2022-05-18 18:31:20 +0300

    ex03

--- day05/script.sh
+++ day05/script.sh
@@ -3,6 +3,5 @@
 python3 -m venv django_venv
 source django_venv/bin/activate
 django_venv/bin/pip3 install --force-reinstall -r requirement.txt
-django-admin startproject Django
-cd Django
-django-admin startapp ex00
+
+
