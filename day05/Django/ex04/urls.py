4a764f5abc43816f1dfe4534a5c336804f7a5f80
Ilnur <ilnrzakirov@gmail.com>
2022-05-19 13:11:13 +0300

    ex04

---
+++ day05/Django/ex04/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('populate/', views.populate_view),
+    path('display/', views.display_view),
+    path('init/', views.db_init_view),
+    path('remove/', views.RemoveView.as_view())
+]
--- day05/Django/ex04/views.py
+++ day05/Django/ex04/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import psycopg2
 from django.conf import settings
 from django.http import HttpRequest, HttpResponse
+from django.views import View
+from .forms import RemoveForm
 
 
 # Create your views here.
@@ -12,11 +14,12 @@ def db_init_view(request):
         connect = psycopg2.connect(dbname=db['NAME'], user=db['USER'], password=db['PASSWORD'],
                                    host=db['HOST'], port=db['PORT'])
         with connect.cursor() as db_connect:
-            db_connect.execute("drop table if exists ex02_movies;")
-            db_connect.execute("CREATE TABLE IF NOT EXISTS ex02_movies (episode_nb INT PRIMARY KEY, "
+            db_connect.execute("drop table if exists ex04_movies;")
+            db_connect.execute("CREATE TABLE IF NOT EXISTS ex04_movies (episode_nb INT PRIMARY KEY, "
                                "title VARCHAR(64) UNIQUE NOT NULL, opening_crawl TEXT, director VARCHAR(32) NOT NULL, "
                                "producer VARCHAR(128) NOT NULL, release_date DATE NOT NULL);")
             connect.commit()
+            connect.close()
             return HttpResponse("Ok")
     except Exception as error:
         return HttpResponse(error)
@@ -28,7 +31,7 @@ def populate_view(request):
         connect = psycopg2.connect(dbname=db['NAME'], user=db['USER'], password=db['PASSWORD'],
                                    host=db['HOST'], port=db['PORT'])
         with connect.cursor() as db_connect:
-            db_connect.execute("INSERT INTO ex02_movies (episode_nb, title, director, producer, "
+            db_connect.execute("INSERT INTO ex04_movies (episode_nb, title, director, producer, "
                                "release_date) VALUES (1, 'The Phantom Menace', 'George Lucas', 'Rick McCallum', "
                                "'1999-05-19'), (2, 'Attack of the Clones', 'George Lucas', 'Rick McCallum', "
                                "'2002-05-16'), (3, 'Revenge of the Sith', 'George Lucas', 'Rick McCallum', "
@@ -51,12 +54,54 @@ def display_view(request):
         connect = psycopg2.connect(dbname=db['NAME'], user=db['USER'], password=db['PASSWORD'],
                                    host=db['HOST'], port=db['PORT'])
         with connect.cursor() as db_connect:
-            db_connect.execute("SELECT * FROM ex02_movies;")
+            db_connect.execute("SELECT * FROM ex04_movies;")
             if (db_connect.fetchone() == None):
                 return HttpResponse("No data available")
-            db_connect.execute("SELECT * FROM ex02_movies;")
+            db_connect.execute("SELECT * FROM ex04_movies;")
             data = db_connect.fetchall()
             connect.close()
     except Exception as error:
         return HttpResponse(error)
     return render(request, 'display.html', context={'data': data})
+
+
+class RemoveView(View):
+
+        def get(self, request):
+            try:
+                db = settings.DATABASES['default']
+                connect = psycopg2.connect(dbname=db['NAME'], user=db['USER'], password=db['PASSWORD'],
+                                           host=db['HOST'], port=db['PORT'])
+                with connect.cursor() as db_connect:
+                    db_connect.execute("SELECT * FROM ex04_movies;")
+                    if (db_connect.fetchone() == None):
+                        return HttpResponse("No data available")
+                    data = db_connect.fetchall()
+                    connect.close()
+                    context = {'form': RemoveForm(choices=((line[0], line[0]) for line in data))}
+                    return render(request, 'remove.html', context=context)
+            except Exception as error:
+                print(error)
+                return HttpResponse("No data available")
+
+        def post(self, request):
+            global connect
+            try:
+                db = settings.DATABASES['default']
+                connect = psycopg2.connect(dbname=db['NAME'], user=db['USER'], password=db['PASSWORD'],
+                                           host=db['HOST'], port=db['PORT'])
+                with connect.cursor() as db_connect:
+                    db_connect.execute("SELECT * FROM ex04_movies;")
+                    data = db_connect.fetchall()
+                    choices = ((line[0], line[0]) for line in data)
+            except Exception as error:
+                print(error)
+                context = RemoveForm(choices, request.POST)
+                if context.is_valid():
+                    try:
+                        with connect.cursor() as db_connect:
+                            db_connect.execute(f"DELETE FROM ex04_movies WHERE title='{context.changed_data['title']}'")
+                            connect.commit()
+                    except Exception as error:
+                        print(error)
+            return redirect('remove.html')
