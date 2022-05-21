ccffe9870b2c328fbd49a906bee51148a505c3ff
Ilnur <ilnrzakirov@gmail.com>
2022-05-21 10:14:22 +0300

    ex00

--- day06/d06/d06/settings.py
+++ day06/d06/d06/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ex'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 42
+
+names = ['anon', 'anon2', 'anon3', 'anon4', 'anon5', 'anon6', 'anon7', 'anon8', 'anon9', 'anon10']
