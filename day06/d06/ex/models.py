a8f5131ba5bfb9bdcc13ba7f74c89de94943c7cc
Ilnur <ilnrzakirov@gmail.com>
2022-05-22 13:30:26 +0300

    ex03

--- day06/d06/ex/models.py
+++ day06/d06/ex/models.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+class UpVoice(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class DownVoice(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Tip(models.Model):
     content = models.TextField(null=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
+    upVoice = models.ManyToManyField(UpVoice)
+    downVoice = models.ManyToManyField(DownVoice)
+
