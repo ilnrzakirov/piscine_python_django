373744121a548ed3b4cc4fb0debf6ef456b27e6b
Ilnur <ilnrzakirov@gmail.com>
2022-05-23 18:23:43 +0300

    ex04

--- day06/d06/ex/admin.py
+++ day06/d06/ex/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tip
 
-# Register your models here.
+
+@admin.register(Tip)
+class TipAdmin(admin.ModelAdmin):
+    list_display = ['content', 'author', 'date', 'countUpVoice', 'countDownVoice']
