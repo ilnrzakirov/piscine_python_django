27378a645eaab75d36f43e60e7c249985483f2b6
bcarlee <bcarlee@student.21-school.ru>
2022-05-24 10:01:12 +0300

    ex00

--- day07/article/article_app/models.py
+++ day07/article/article_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=64, null=False)
-    author = models.ForeignKey(User, null=False)
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     created = models.DateTimeField(null=False, auto_now_add=True)
     synopsis = models.CharField(null=False, max_length=312)
     content = models.TextField(null=False)
@@ -13,8 +13,8 @@ class Article(models.Model):
 
 
 class UserFavouriteArticle(models.Model):
-    user = models.ForeignKey(User, null=False)
-    article = models.ForeignKey(Article, null=False)
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, null=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.article.title
